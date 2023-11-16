c2684fc10709442b71c5a9c4586aeaf987ae4bf4
vicky kumar mishra <mishravicky0141@gmail.com>
2023-11-17 01:18:18 +0530

    Update Navbar.js

--- profile/src/Navbar.js
+++ profile/src/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
       <nav class="navbar bg-body-black" style={{backgroundColor:"black"}}>
   <div class="container-fluid">
     
-    <a class="navbar-brand" style={{color:"white"}}><img src="tiny tales logo.png" style={{height:'2.5rem', width:'2.5rem'}}></img>|<b>STORIES</b></a>
+    <a class="navbar-brand" style={{color:"white"}}><img src="tiny tales logo.png" style={{height:'2.5rem', width:'2.5rem'}}></img>|<b style={{textShadow: '4px 4px 4px blue'}}>STORIES</b></a>
     <form class="d-flex" role="search">
       
       <button class="btn btn-outline-success" type="submit" style={{color:'black',backgroundColor:'#fcba03'}}><b>Courses</b></button>
